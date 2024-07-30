d725c6f4ff11460ffc20d007cb0852918278902a
jun6292 <c68254@gmail.com>
2024-07-30 13:05:07 +0900

    Feat: MySelectShop 프로젝트 구현

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(
+            @RequestBody ProductRequestDto productRequestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return productService.createProduct(productRequestDto, userDetails.getUser());
+    }
+
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto productMypriceRequestDto) {
+        return productService.updateProduct(id, productMypriceRequestDto);
+    }
+
+    @GetMapping("/products")
+    public Page<ProductResponseDto> getProducts(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+
+    ) {
+        return productService.getProducts(
+                userDetails.getUser(), page - 1, size, sortBy, isAsc
+        );
+    }
+
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(@PathVariable Long productId,
+                          @RequestParam Long folderId,
+                          @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsIn(
+            @PathVariable Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        return productService.getProductsIn(folderId, userDetails.getUser(), page - 1, size, sortBy, isAsc);
+    }
+}
