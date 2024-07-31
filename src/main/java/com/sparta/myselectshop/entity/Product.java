ed1fc8725ef08374f78ec95220ff213aa6072e61
jun6292 <c68254@gmail.com>
2024-08-01 00:07:07 +0900

    Feat: 카카오 로그인 정보로 회원가입 구현

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
@@ -41,7 +42,7 @@ public class Product extends Timestamped {
     private User user;
 
     @OneToMany(mappedBy = "product")
-    private List<ProductFolder> productFolderList;
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
