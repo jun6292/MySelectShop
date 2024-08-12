e5ddb35cf7e131888f302bb22e782d78a139149e
jun6292 <c68254@gmail.com>
2024-08-12 15:23:16 +0900

    CI/CD 구성 - 데이터베이스 rds로 변경

--- src/main/java/com/sparta/myselectshop/config/JpaConfig.java
+++ src/main/java/com/sparta/myselectshop/config/JpaConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing // 시간 자동 변경 가능
+@EnableJpaAuditing
 public class JpaConfig {
 }
